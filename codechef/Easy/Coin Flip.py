507252d75f4717bdc6dc8cac668b1065a85b6181
Abhishek <abhisheksontakke92@gmail.com>
2020-01-03 00:07:48 +0530

    Day 6

--- codechef/Easy/Coin Flip.py
+++ codechef/Easy/Coin Flip.py
@@ -3,19 +3,7 @@ for i in range(t):
     g = int(input())
     for j in range(g):
         i,n,q = map(int,input().split())
-        if(i == 1):
-            if(n%2 == 1):
-                if(q == 1):
-                    print(n//2)
-                else:
-                    print((n+1)//2)
-            else:
-                print(n//2)
-        else:            
-            if(n%2 == 1):
-                if(q == 1):
-                    print((n+1)//2)
-                else:
-                    print(n//2)
-            else:
-                print(n//2)
+        if(i == q):
+            print(n//2)
+        else:
+            print((n+1)//2)
