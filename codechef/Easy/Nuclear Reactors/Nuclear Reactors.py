20a160ca79053246a75f118ed989a1065cf4b7de
Abhishek <abhisheksontakke92@gmail.com>
2020-01-08 09:02:40 +0530

    Day 11 - Incomplete Nuclear Reactor

--- codechef/Easy/Nuclear Reactors/Nuclear Reactors.py
+++ codechef/Easy/Nuclear Reactors/Nuclear Reactors.py
@@ -2,17 +2,11 @@ ab,n,k = map(int,input().split())
 a = [0]*k
 
 def change(i,j):
-    #set = 0
-    #while(set == 0):
     if(a[i+j] > n):
-        #a[i+j]+=1
-        #j+=1
         change(i,(j+1))
         a[i+j+1] = 0
     else:
         a[i+1]= a[i+1] + 1
-    
-    
 
 for k in range(ab):
     i = 0
@@ -20,29 +14,6 @@ for k in range(ab):
     if(a[i]+1 > n):
         print(a)
         change(i,1)
-        set = 0
-        j = 1
-        '''
-        while(set == 0):
-            if(a[i+j] > n):
-                a[i+j]+=1
-                j+=1
-            else:
-                a[i+1]= a[i+1] + 1
-                set = 1
-        
-        while(j >= 1):
-            a[i+j] = 0
-            j-=1
-            
-        if(a[i+1] > n):
-            a[i+2]+=1
-            if(a[i+2] > n):
-                a[i+3]+=1
-                a[i+2]=0
-            a[i+1]=0
-        a[i] = 0
-        '''
     else:
         a[i] = a[i] + 1
         
