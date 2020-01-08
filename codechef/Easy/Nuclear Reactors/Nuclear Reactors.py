a34a2c79c9f95e8ffbcad04f219767ce4ad16ff1
Abhishek <abhisheksontakke92@gmail.com>
2020-01-08 23:53:42 +0530

    Day 11 - Nuclear Reactor

--- codechef/Easy/Nuclear Reactors/Nuclear Reactors.py
+++ codechef/Easy/Nuclear Reactors/Nuclear Reactors.py
@@ -1,12 +1,10 @@
-ab,n,k = map(int,input().split())
-a = [0]*k
-for i in range(ab):
-    a[0] = a[0] + 1
-    while(max(a) == (n+1)):
-        i = a.index(max(a))
-        a[i] = 0
-        a[i+1] = a[i+1] + 1
+import sys
+ab,n,k = map(int,sys.stdin.readline().split())
+a = []
+for i in range(k):
+    a.append(ab%(n+1))
+    ab = ab//(n+1)
 res = ""
 for i in a:
-    res += str(i)+" " 
+    res+= str(i) + " "
 print(res)
