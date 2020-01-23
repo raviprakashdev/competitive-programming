fe7f154218d748116141bbdb014c94495f56991b
Abhishek <abhisheksontakke92@gmail.com>
2020-01-23 21:52:06 +0530

    Day 26 - Codechef- Chef and right triangles

--- codechef/Easy/Chef and The Right Triangles/Chef and The Right Triangles.py
+++ codechef/Easy/Chef and The Right Triangles/Chef and The Right Triangles.py
@@ -1,27 +1,12 @@
-def slope(x1,x2,y1,y2):
-    if(x2 - x1 == 0):
-        return 0
-    else:
-        m = (y2 - y1)/(x2 - x1)
-        return m
-
-n = int(input())
-Count = 0
-
-for i in range(n):
+def distance(x1,y1,x2,y2):
+    return (x1-x2)**2 + (y1 -y2 )**2
+    
+count = 0
+for _ in range(int(input())):
     x1,y1,x2,y2,x3,y3 = map(int,input().split())
-    m1 = slope(x1,x2,y1,y2)
-    m2 = slope(x1,x3,y1,y3)
-    m3 = slope(x2,x3,y2,y3)
-    m1m2 = m1*m2  
-    if(m1m2  == -1 or  (m1 == 0 and m2 == 0)):
-        Count+=1
-    else:
-        m1m2 = m2*m3  
-        if(m1m2  == -1 or  (m2 == 0 and m3 == 0)):
-            Count+=1
-        else:
-            m1m2 = m1*m3  
-            if(m1m2  == -1 or  (m1 == 0 and m3 == 0)):
-                Count+=1
-print(Count)
+    m1 = distance(x1,y1,x2,y2)
+    m2 = distance(x2,y2,x3,y3)
+    m3 = distance(x3,y3,x1,y1)
+    if(m1 + m2 == m3 or m1 + m3 == m2 or m2 + m3 == m1):
+        count+=1
+print(count)
