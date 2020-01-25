c766acd54592338ffb97b42190171c49e290fadd
Abhishek <abhisheksontakke92@gmail.com>
2020-01-26 00:18:51 +0530

    Day 29 - Codechef- Jan Lunchtime

--- codechef/January Lunchtime/Bob and His Friends.py
+++ codechef/January Lunchtime/Bob and His Friends.py
@@ -1,17 +1,7 @@
 for i in range(int(input())):
     n,a,b,c = map(int,input().split())
     f = list(map(int, input().split()))
-    t = 0
-    if( (min(f) < b and max(f) < b) and (min(f) > a and max(f) > a) or (min(f) > b and max(f) > b) and (min(f) < a and max(f) < a) ):
-        t = abs(b-a) + c
-    else:
-        m = (a+b)/2
-        p = []
-        for i in range(n):
-            p.append(f[i] - m)
-        p = list(p)
-        val = p[0] + m
-        minp = abs(val - a)
-        maxp = abs(val - b)
-        t = maxp + minp + c
+    t = 10**18
+    for i in range(n):
+        t = min(t, abs(f[i] - a) + abs(f[i] - b) + c)
     print(int(t))
